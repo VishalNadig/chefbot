49c848b18eb382e8ae30adced30658af682590ab
Vishal Nadig <nadigvishal@gmail.com>
2022-12-26 11:32:44 -0700

    Added API calls to chefbot code

--- chefbot.py
+++ chefbot.py
@@ -1,49 +1,66 @@
 import argparse
+import requests
 from pprint import pprint
-
-import pandas as pd
-
+import os
 arguments = argparse.ArgumentParser()
 
 arguments.add_argument('-d', '--dishname', required= True, type= str, help= "A general word of what you want to eat. Ex salad, pasta, pizza.")
+arguments.add_argument('-f', '--favorites', type= bool, help= "A general word of what you want to eat. Ex salad, pasta, pizza.")
+
 
 args = arguments.parse_args()
-pd.set_option("display.max_colwidth", None)
-csv_file = r"/home/megamind/git/github/personal/chefbot/Modified_Indian_Food_Dataset.csv"
-markdown_file_path = "/home/megamind/chefbot/"
+markdown_file_path = r"/Users/akshathanadig/Downloads/git/chefbot/recipes/"
+favourites_folder = r"/Users/akshathanadig/Downloads/git/chefbot/favourite_recipes/"
+def fetch_the_menu(dish: str):
+    headers = {
+    'accept': 'application/json',
+    }
+    params = {
+        'dish': dish,
+    }
+    response = requests.get('http://192.168.0.36:6969/chefbot/get_dishes', params=params, headers=headers)
+    pprint(response.content.decode("utf-8"))
 
-def get_dataframe():
-    file = pd.read_csv(csv_file)
-    return file
+def fetch_recipe(dish: str, index: int):
+    headers = {
+        'accept': 'application/json',
+        'content-type': 'application/x-www-form-urlencoded',
+    }
+
+    params = {
+        'dish': dish,
+        'index_number': index,
+    }
+
+    response = requests.post('http://192.168.0.36:6969/chefbot/fetch_recipe', params=params, headers=headers)
+    pprint(response.text)
 
 
-def search_dataset(dish: str):
-    count = 1
-    dish_dictionary = {}
-    df = get_dataframe()
-    for dishes in df["TranslatedRecipeName"]:
-        if dish.title() in dishes:
-            dish_dictionary[count] = []
-            dish_dictionary[count].append(dishes)
-            count += 1
-            # with open("found_dishes.md", 'w') as file:
-            #     file.write(str(dishes))
-    if len(dish_dictionary) > 0:
-        print(f"There are {count-1} different {dish} recipies we found! They are:\n")
-        pprint(dish_dictionary)
-        choice = input("\n\nChoose the number for which we should prepare the order: ")
-        chosen_dish = dish_dictionary[int(choice)][0]
-        print(f"""You chose to make: {chosen_dish}\n""")
-        df_new = df[df["TranslatedRecipeName"] == chosen_dish]
-        index = df_new.index[0]
-        dictionary = df_new.to_dict()
-        with open(
-            rf"{markdown_file_path}/{chosen_dish}.md",
-            "w",
-        ) as file:
-            file.write(
-                f"""# {chosen_dish}\n\n## Cooking time: {dictionary['TotalTimeInMins'][index]} minutes.\n\n## Ingredients:\n{dictionary['Cleaned-Ingredients'][index]}\n\n## Cooking Instructions:\n{dictionary['TranslatedInstructions'][index]}"""
-            )
-    else:
-        print("Sorry, we don't have what you are looking for!")
-search_dataset(args.dishname)
+def download_recipie(dish: str, index: int, favourite: bool = False):
+    headers = {
+    'accept': 'application/json',
+    'content-type': 'application/x-www-form-urlencoded',
+    }
+    params = {
+        'dish': dish,
+        'index_number': index,
+    }
+
+    response = requests.post('http://192.168.0.36:6969/chefbot/download_recipe', params=params, headers=headers)
+    print(response.text)
+    with open(markdown_file_path+f"{dish}.md", "w") as file:
+        file.write(response.text)
+    if favourite:
+        with open(favourites_folder+f"{dish}.md", "w") as file:
+            file.write(response.text)
+
+def cook(dish: str, index: int):
+    recipe = fetch_recipe(dish=dish, index=index)
+    clear_the_table()
+
+def clear_the_table():
+    for file in os.listdir(markdown_file_path):
+        os.remove(os.path.join(markdown_file_path, file))
+
+download_recipie(args.dishname, 1)
+clear_the_table()
